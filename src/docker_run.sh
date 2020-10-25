1b7a862052699c035a877934262e8a507bb2f7e4
ababa831 <flvonlineconverter@gmail.com>
2020-10-26 02:41:17 +0900

    Fix some errors

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -2,4 +2,22 @@
 IEX_TOKEN=$(gcloud secrets versions access 1 --secret IEX_TOKEN)
 SLACK_TOKEN=$(gcloud secrets versions access 1 --secret SLACK_TOKEN)
 
-docker run --rm -it --env IEX_TOKEN=$IEX_TOKEN --env SLACK_TOKEN=$SLACK_TOKEN minervini_screening:latest --country ja
+ja_searchtime_start=$(date "+%Y-%m-%d 16:00:00")
+ja_searchtime_end=$(date "+%Y-%m-%d 23:59:59")
+us_searchtime_start=$(date "+%Y-%m-%d 09:00:00")
+us_searchtime_end=$(date "+%Y-%m-%d 15:59:59")
+
+datenow=$(date "+%Y-%m-%d %H:%M:%S")
+
+country="None"
+if [[ "$ja_searchtime_start" < "$datenow" ]] && [[ "$ja_searchtime_end" > "$datenow" ]]; then
+    country="ja"
+elif [[ "$us_searchtime_start" < "$datenow" ]] && [[ "$us_searchtime_end" > "$datenow" ]]; then
+    country="us"
+fi
+
+echo $country
+
+if [ "$country" != "None" ]; then
+    docker run --rm -it --env IEX_TOKEN=$IEX_TOKEN --env SLACK_TOKEN=$SLACK_TOKEN minervini_screening:latest --country $country
+fi
